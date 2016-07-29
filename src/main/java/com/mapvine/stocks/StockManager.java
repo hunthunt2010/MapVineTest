38024c01c9558de1d789f6f0f8c217cfa21efff4
David Hunter <david@openbookben.com>
2016-07-29 14:11:34 -0600

    Got all tests passing

--- src/main/java/com/mapvine/stocks/StockManager.java
+++ src/main/java/com/mapvine/stocks/StockManager.java
@@ -1,36 +1,57 @@
 package com.mapvine.stocks;
 
+import com.mapvine.stocks.model.SellDetails;
 import com.mapvine.stocks.model.Stock;
-
+import com.mapvine.stocks.model.StockDetails;
 import java.math.BigDecimal;
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class StockManager {
+
+    private Map<String, List<StockDetails>> stockTickerToAmountOwned = new HashMap<>();
+
+    private static String formatLookupString(String stringToFormat) {
+        if (stringToFormat != null) {
+            stringToFormat = stringToFormat.replaceAll("\\s", "").toUpperCase();
+        }
+
+        return stringToFormat;
+    }
 
     /**
      * Get the list of stocks currently under management by its ticker symbol.
      */
     public List<Stock> findByTicker(final String ticker) {
-        // TODO: Implement me.
-        return null;
+        String lookupTicker = formatLookupString(ticker);
+
+        List<StockDetails> stockDetails = stockTickerToAmountOwned.getOrDefault(lookupTicker, new ArrayList<>());
+        return stockDetails.stream().map(StockDetails::getStock).collect(Collectors.toList());
     }
 
     /**
      * For a given ticker symbol, add up the total stock values under management and return.
      */
     public BigDecimal getValueUnderManagerByTicker(final String ticker) {
-        // TODO: Implement me.
-        return null;
+        List<StockDetails> details = stockTickerToAmountOwned.get(formatLookupString(ticker));
+        if (details == null) {
+            return new BigDecimal(0);
+        }
+        return details.stream().map(d -> {
+            BigDecimal multiply = d.getStock().getPrice().multiply(new BigDecimal(d.getSharesOwned()));
+            return multiply;
+        }).reduce(BigDecimal::add).get().stripTrailingZeros();
     }
 
     /**
      * For a given ticker symbol, get the number of stocks under management.
      */
     public int numberOfStocksByTicker(final String ticker) {
-        // TODO: Implement me.
-        return -1;
+        List<StockDetails> details = stockTickerToAmountOwned.get(formatLookupString(ticker));
+        if (details == null) {
+            return 0;
+        }
+        return details.stream().map(StockDetails::getSharesOwned).reduce(Integer::sum).get();
     }
 
     /**
@@ -41,7 +62,18 @@ public class StockManager {
      * @throws java.lang.IllegalArgumentException if numberOfShares is <= 0
      */
     public void buyStock(final Stock stock, final int numberOfShares) {
-        // TODO: Implement me.
+        if (numberOfShares <= 0) {
+            throw new IllegalArgumentException("Invalid number of stocks to buy");
+        }
+        String ticker = stock.getTicker();
+        List<StockDetails> stockDetails = stockTickerToAmountOwned.get(ticker);
+        if (stockDetails == null) {
+            List<StockDetails> newStockDetails = new ArrayList<>();
+            newStockDetails.add(new StockDetails(stock, numberOfShares));
+            stockTickerToAmountOwned.put(ticker,  newStockDetails);
+        } else {
+            stockDetails.add(new StockDetails(stock, numberOfShares));
+        }
     }
 
     /**
@@ -56,8 +88,48 @@ public class StockManager {
      * @throws java.lang.IllegalArgumentException if numberOfShares is <= 0
      */
     public Optional<Set<BigDecimal>> sellStock(final String ticker, final int numberOfShares, final BigDecimal sharePrice) {
-        // TODO: Implement me.
-        return null;
+        if (numberOfShares <= 0) {
+            throw new IllegalArgumentException("Invalid number of stocks to buy");
+        }
+
+        List<StockDetails> details = stockTickerToAmountOwned.get(formatLookupString(ticker));
+        if (details == null) {
+            return Optional.empty();
+        }
+        Collections.sort(details, new Comparator<StockDetails>() {
+            @Override
+            public int compare(StockDetails o1, StockDetails o2) {
+                return o1.getStock().getPrice().compareTo(o2.getStock().getPrice());
+            }
+        });
+        Set<BigDecimal> sellPrices = new HashSet<>();
+        int numberOfSharesSatisfied = 0;
+        for (StockDetails detail : details) {
+            int sharesThatNeedToBeFilled = numberOfShares - numberOfSharesSatisfied;
+            int numberOfSharesOwned = detail.getSharesOwned();
+            if (sharesThatNeedToBeFilled >= numberOfSharesOwned) {
+                numberOfSharesSatisfied += numberOfSharesOwned;
+                detail.setSellDetails(new SellDetails(sharePrice, numberOfSharesOwned));
+            } else {
+                detail.setSellDetails(new SellDetails(sharePrice, sharesThatNeedToBeFilled));
+            }
+            sellPrices.add(detail.getStock().getPrice());
+        }
+        Optional<Set<BigDecimal>> retVal = Optional.of(sellPrices);
+        retVal.ifPresent(d -> {
+            for (StockDetails detail : details) {
+                if (detail.getSellDetails() != null) {
+                    int sharesSold = detail.getSellDetails().getSharesToSell();
+                    int sharesAfterSell = detail.getSharesOwned() - sharesSold;
+                    detail.setSharesOwned(sharesAfterSell);
+                    BigDecimal amountFromSale = detail.getSellDetails().getSellPrice().multiply(new BigDecimal(sharesSold));
+                    BigDecimal value = detail.getStock().getPrice().multiply(new BigDecimal(sharesSold));
+                    detail.setProfitLoss(amountFromSale.subtract(value));
+                    detail.setSellDetails(null);
+                }
+            }
+        });
+        return retVal;
     }
 
     /**
@@ -66,7 +138,10 @@ public class StockManager {
      * @return The total profit made so far or an empty option if the stock is not under management
      */
     public Optional<BigDecimal> getProfitForStockByTicker(final String ticker) {
-        // TODO: Implement me.
-        return null;
+        List<StockDetails> details = stockTickerToAmountOwned.get(formatLookupString(ticker));
+        if (details == null) {
+            return Optional.empty();
+        }
+        return Optional.of(details.stream().map(StockDetails::getProfitLoss).reduce(BigDecimal::add).get());
     }
 }
